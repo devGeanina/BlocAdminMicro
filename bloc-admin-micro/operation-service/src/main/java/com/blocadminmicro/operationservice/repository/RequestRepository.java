156d3e80b02d13b8137122ded600dfe9b05de59b
devGeanina <35954631+devGeanina@users.noreply.github.com>
2021-06-11 18:13:36 +0300

    Update RequestRepository.java

--- bloc-admin-micro/operation-service/src/main/java/com/blocadminmicro/operationservice/repository/RequestRepository.java
+++ bloc-admin-micro/operation-service/src/main/java/com/blocadminmicro/operationservice/repository/RequestRepository.java
@@ -3,8 +3,9 @@ package com.blocadminmicro.operationservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blocadminmicro.operationservice.entity.Request;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface RequestRepository extends JpaRepository<Request, Long>{
 
 }
