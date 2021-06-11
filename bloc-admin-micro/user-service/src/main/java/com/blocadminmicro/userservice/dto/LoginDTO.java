28bd9a30a6f6e7e2cf6f52b76cc4eece67f9c342
devGeanina <35954631+devGeanina@users.noreply.github.com>
2021-06-11 18:08:23 +0300

    Update LoginDTO.java

--- bloc-admin-micro/user-service/src/main/java/com/blocadminmicro/userservice/dto/LoginDTO.java
+++ bloc-admin-micro/user-service/src/main/java/com/blocadminmicro/userservice/dto/LoginDTO.java
@@ -2,9 +2,7 @@ package com.blocadminmicro.userservice.dto;
 
 import java.io.Serializable;
 
-import lombok.Data;
 
-@Data
 public class LoginDTO implements Serializable{
 
 	private static final long serialVersionUID = 1L;
