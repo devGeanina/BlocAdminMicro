2bc591563f223b22e476a085f2a65584403cf4de
devGeanina <35954631+devGeanina@users.noreply.github.com>
2021-06-11 18:12:52 +0300

    Update BudgetRepository.java

--- bloc-admin-micro/budget-service/src/main/java/com/blocadminmicro/budgetservice/repository/BudgetRepository.java
+++ bloc-admin-micro/budget-service/src/main/java/com/blocadminmicro/budgetservice/repository/BudgetRepository.java
@@ -3,7 +3,9 @@ package com.blocadminmicro.budgetservice.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blocadminmicro.budgetservice.entity.Budget;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long>{
 
 }
