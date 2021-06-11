2a3536b0eb3defcd2e35054c2d3b6f280b84168e
devGeanina <35954631+devGeanina@users.noreply.github.com>
2021-06-11 18:13:13 +0300

    Update ExpenseRepository.java

--- bloc-admin-micro/operation-service/src/main/java/com/blocadminmicro/operationservice/repository/ExpenseRepository.java
+++ bloc-admin-micro/operation-service/src/main/java/com/blocadminmicro/operationservice/repository/ExpenseRepository.java
@@ -4,7 +4,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.blocadminmicro.operationservice.entity.Expense;
 
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface ExpenseRepository extends JpaRepository<Expense, Long>{
 
 }
