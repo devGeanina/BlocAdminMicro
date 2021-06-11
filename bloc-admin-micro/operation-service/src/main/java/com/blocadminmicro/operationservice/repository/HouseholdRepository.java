bbc775e5f12ca1555d3d2412202b6b63797c551d
devGeanina <35954631+devGeanina@users.noreply.github.com>
2021-06-11 18:13:24 +0300

    Update HouseholdRepository.java

--- bloc-admin-micro/operation-service/src/main/java/com/blocadminmicro/operationservice/repository/HouseholdRepository.java
+++ bloc-admin-micro/operation-service/src/main/java/com/blocadminmicro/operationservice/repository/HouseholdRepository.java
@@ -5,9 +5,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import com.blocadminmicro.operationservice.entity.Household;
+import org.springframework.stereotype.Repository;
 
-
-
+@Repository
 public interface HouseholdRepository extends JpaRepository<Household, Long>{
 
 	@Query("SELECT DISTINCT h FROM Household h LEFT JOIN h.expenses expense where expense.leftoverSum > 0 and expense.payedInFull = false")
